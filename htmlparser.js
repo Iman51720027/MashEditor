ba303237a5a441de7abcb6f2970251158c6421d8
Bijkar program YT <sikdariman82@gmail.com>
2026-09-23 13:00:29 +0530

    Add files via upload

--- htmlparser.js
+++
@@ -1,7 +0,0 @@
-var textEditor = document.getElementById("textBox");
-var runBtn = document.getElementById("runBtn");
-var outputBox = document.getElementById("outputBox");
-
-runBtn.addEventListener('click', function() {
-  outputBox.innerHTML = textEditor.value;
-})
--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-"use strict";
-var x = 10;
-x = "iman";
-console.log(x);
+// textBox var 
+var textEditor = document.getElementById("textBox");
+// run btn var 
+var runBtn = document.getElementById("runBtn");
+// outputBox var
+var outputBox = document.getElementById("outputBox");
+// run btn event
+runBtn.addEventListener('click', function() {
+  // code var
+  var code = textEditor.value;
+  // bik se suru hota hei to
+  if (code.startsWith("bik")) {
+    outputBox.textContent = code.substring(4); // (4) kata jayega
+  }
+  else { 
+    // error message
+    outputBox.textContent = "syntax error: invalid code";
+  }
+})
