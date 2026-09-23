21f3b1c04eee030eb6c39f4816cbe8c936bebe5a
Bijkar program YT <sikdariman82@gmail.com>
2026-09-23 09:21:11 +0530

    Add files via upload

--- htmlparser.js
+++ htmlparser.js
@@ -1,3 +1,7 @@
 var textEditor = document.getElementById("textBox");
 var runBtn = document.getElementById("runBtn");
 var outputBox = document.getElementById("outputBox");
+
+runBtn.addEventListener('click', function() {
+  outputBox.innerHTML = textEditor.value;
+})
